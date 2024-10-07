b3f832187c6aaca4be864c2734bb00be09df75ad
gustavo <gustavo@phntms.com>
2024-10-07 16:42:19 +0100

    Update constats

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,6 @@
 import os
 
+GAE_PROJECT = os.getenv('GOOGLE_CLOUD_PROJECT')
 GAE_ENV = os.environ.get('GAE_ENV')
 IS_LOCAL = (GAE_ENV is None or GAE_ENV == 'localdev')
 LOCAL_DEV = os.environ.get('LOCAL_DEV', None)
