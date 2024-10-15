d7609f80a80f1ab30820ace384f9860e9a2a7674
Gustavo <gustavo.passarella@hotmail.com>
2024-10-15 08:49:32 +0100

    Add login template

--- src/routes/general.py
+++ src/routes/general.py
@@ -1,6 +1,7 @@
 import flask
 
 from src.utils import db_models
+from src.utils.services import auth
 
 ROUTES = flask.Blueprint('general', __name__)
 
@@ -13,11 +14,21 @@ def warmup() -> str:
 
 @ROUTES.route('/')
 def home() -> str:
-  return flask.render_template('index.html')
+  client_id = auth.get_client_id()
+  return flask.render_template('login.html', client_id=client_id)
 
 
-@ROUTES.route('/add')
-def test_route() -> str:
-  user = db_models.User(email_address='test@test.com').put()
-  print(user)
-  return flask.render_template('index.html')
+@ROUTES.route("/logged_in")
+def logged_in():
+  # Check if user is logged in
+  if 'user_id' not in flask.session:
+    return flask.redirect(flask.url_for('index'))
+
+  return flask.render_template("index.html", name=flask.session['name'])
+
+
+# @ROUTES.route('/add')
+# def test_route() -> str:
+#   user = db_models.User(email_address='test@test.com').put()
+#   print(user)
+#   return flask.render_template('index.html')
