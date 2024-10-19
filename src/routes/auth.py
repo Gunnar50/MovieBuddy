026638da9c71c5892947387c24d56c098759b44c
Gustavo <gustavo.passarella@hotmail.com>
2024-10-19 07:43:54 +0100

    Update session

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -14,21 +14,23 @@ ROUTES = flask.Blueprint('auth', __name__, url_prefix='/user/api/auth')
 def login():
   token = flask.request.json.get('token')
   if not token:
-    return flask.jsonify({'error': 'No token provided'}), 400
+    return flask.jsonify({'error': 'No token provided'})
 
   user_info = auth.login(token)
-  # flask.session[constants.SESSION_USER_ID] = user_info.id
+  flask.session[constants.SESSION_USER_ID] = user_info.id
+  flask.session['name'] = user_info.name
 
   return flask.jsonify({
       "message": "Login successful",
       "user_id": user_info.id,
       "email": user_info.email_address,
       "name": user_info.name,
+      "success": True,
   })
 
 
-@ROUTES.route("/logout")
+@ROUTES.route("/logout", methods=('POST',))
 def logout():
   # Clear the session
-  flask.session.pop(constants.SESSION_USER_ID)
-  return flask.redirect(flask.url_for('index'))
+  flask.session.clear()
+  return flask.redirect(flask.url_for('general.home'))
