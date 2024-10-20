15f7b458c24bb87a87de705a8c13281fa0fcdbc4
Gustavo <gustavo.passarella@hotmail.com>
2024-10-20 16:48:40 +0100

    Update Apis

--- src/utils/api.py
+++ src/utils/api.py
@@ -1,15 +1,28 @@
+import enum
 import pydantic
+
+
+class MemberType(enum.Enum):
+  OWNER = 'owner'
+  MEMBER = 'member'
 
 
 class MemberDetail(pydantic.BaseModel):
   name: str
   email_address: str
+  member_type: MemberType
 
 
 class ListInfo(pydantic.BaseModel):
   name: str
   owner_id: int
   shared: list[MemberDetail]
+
+
+class WatchlistDetail(pydantic.BaseModel):
+  watchlist_id: int
+  title: str
+  description: str
 
 
 class ListsInfo(pydantic.BaseModel):
---
+++ src/utils/services/serialisers.py
@@ -0,0 +1,6 @@
+from utils import api, db_models
+
+
+def serialise_watchlist_details(
+    watchlist: db_models.Watchlist) -> api.WatchlistDetail:
+  pass
