0879bde2867778fcd458ef8154c614c59b28e2fb
gustavo <gustavo@phntms.com>
2024-10-21 09:10:04 +0100

    Add serialisers for watchlist details and response

--- src/utils/services/serialisers.py
+++ src/utils/services/serialisers.py
@@ -3,4 +3,31 @@ from utils import api, db_models
 
 def serialise_watchlist_details(
     watchlist: db_models.Watchlist) -> api.WatchlistDetail:
-  pass
+  return api.WatchlistDetail(
+      watchlist_id=watchlist.key.id(),
+      title=watchlist.title,
+      description=watchlist.description,
+  )
+
+
+def serialise_watchlist_response(
+    watchlist: db_models.Watchlist,
+    owner: db_models.WatchlistOwner,
+    members: list[db_models.WatchlistMember],
+) -> api.WatchlistResponse:
+  sorted_members = sorted(members, key=lambda member: member.email)
+  return api.WatchlistResponse(
+      watchlist=serialise_watchlist_details(watchlist),
+      owner=api.MemberDetail(
+          name=owner.name,
+          email=owner.email,
+          member_type=api.MemberType.OWNER,
+      ),
+      members=[
+          api.MemberDetail(
+              name=member.name,
+              email=member.email,
+              member_type=api.MemberType.MEMBER,
+          ) for member in sorted_members
+      ],
+  )
