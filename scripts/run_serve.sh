440f36e185b1d379c4ac6b1aa079c67e4d0e8cc6
gustavo <gustavo@phntms.com>
2024-10-25 14:40:40 +0100

    Update serve command

--- scripts/run_serve.sh
+++ scripts/run_serve.sh
@@ -5,7 +5,7 @@ set -o errexit   # Fail on any error
 set -o pipefail  # Trace ERR through pipes
 set -o errtrace  # Trace ERR through sub-shell commands
 
-dev_appserver.py \
+cd src && dev_appserver.py \
     app.yaml \
     --application=moviebuddy-437608 \
     --enable_console \
