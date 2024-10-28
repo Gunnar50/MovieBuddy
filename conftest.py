8ce1cb7c616fcb1b2256b73040fe4bf051254b55
Gustavo <gustavo.passarella@hotmail.com>
2024-10-28 06:13:54 +0000

    Move conftest to root

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Add the src directory to sys.path
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+pytest_plugins = 'src.utils.fixtures'
