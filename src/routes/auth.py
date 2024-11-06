5c8d791f05f48d405dcea69dd0030a21ce29007c
gustavo <gustavo@phntms.com>
2024-11-06 15:05:23 +0000

    Update login route

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -17,26 +17,25 @@ def login():
   user_info = auth.login(body.access_token)
 
   user_profile = db_models.UserProfile.get_by_id(user_info.google_id)
+  # TODO - Update user profile in case something has changed.
   if not user_profile:
-    new_user = db_models.UserProfile(id=user_info.google_id,
-                                     email_address=user_info.email_address,
-                                     name=user_info.name,
-                                     avatar=user_info.avatar)
-    new_user.put()
+    db_models.UserProfile(
+        id=user_info.google_id,
+        email_address=user_info.email_address,
+        name=user_info.name,
+        avatar=user_info.avatar,
+    ).put()
 
   flask.session[constants.SESSION_USER_ID] = user_info.google_id
 
-  return flask.jsonify({
-      "message": "Login successful",
-      "user_id": user_info.id,
-      "email": user_info.email_address,
-      "name": user_info.name,
-      "success": True,
-  })
+  return api.UserDetails(
+      user_id=user_info.google_id,
+      email=user_info.email_address,
+      name=user_info.name,
+  )
 
 
-@ROUTES.route("/logout", methods=('POST',))
-def logout():
-  # Clear the session
-  flask.session.clear()
-  return flask.redirect(flask.url_for('general.home'))
+@ROUTES.route('/logout', methods=('POST',))
+def logout() -> str:
+  auth.logout()
+  return 'Ok'
