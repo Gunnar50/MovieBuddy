9643358c16882d6c805c229c50ea1f9a47267c02
Gustavo <gustavo.passarella@hotmail.com>
2024-11-10 20:36:24 +0000

    Move backend to its folder

--- src/ts/index.ts
+++
@@ -1,9 +0,0 @@
-const initializeApp = () => {
-  window.addEventListener('DOMContentLoaded', () => {
-    console.log("Hello, Typescript")
-    // import code
-    // import('@/view-manager');
-  });
-};
-
-document.addEventListener('DOMContentLoaded', initializeApp);
