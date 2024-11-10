885086ec020283495e43e7fe5a90c0d3363e4ed9
Gustavo <gustavo.passarella@hotmail.com>
2024-11-10 20:56:08 +0000

    Fix scripts and conftest

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,8 +1,1 @@
-import os
-import sys
-
-# Add the src directory to sys.path
-sys.path.insert(0,
-                os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
-
-pytest_plugins = 'src.utils.fixtures'
+pytest_plugins = 'utils.fixtures'
--- scripts/check_types.sh
+++ scripts/check_types.sh
@@ -7,4 +7,4 @@ set -o errexit   # Fail on any error
 set -o pipefail  # Trace ERR through pipes
 set -o errtrace  # Trace ERR through sub-shell commands
 
-../scripts/poetry_run.sh pytype --config=./pytype.cfg -o ./.pytype $@
+../scripts/poetry_run.sh pytype --config=../pytype.cfg -o ./.pytype $@
