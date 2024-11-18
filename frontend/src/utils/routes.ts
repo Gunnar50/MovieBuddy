3d6266704939c990820da5a9ade197bb61f9de69
gustavo <gustavo.passarella@hotmail.com>
2024-11-18 16:33:10 +0000

    Add routes file

---
+++ frontend/src/utils/routes.ts
@@ -0,0 +1,7 @@
+export enum ApplicationRoute {
+  LOGIN = 'admin',
+  LOGGED_IN = 'logged_in',
+}
+
+export const LOGIN_PATH = `${ApplicationRoute.LOGIN}`;
+export const LOGGED_IN_PATH = `${ApplicationRoute.LOGGED_IN}`;
