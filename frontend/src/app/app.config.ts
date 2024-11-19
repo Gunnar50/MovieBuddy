fc0599fd9dae8de84f7199c1e207d9fb5dd738ae
gustavo <gustavo.passarella@hotmail.com>
2024-11-19 11:17:34 +0000

    Move providers to config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,35 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withRouterConfig} from '@angular/router';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 import {routes} from './app.routes';
+import {AuthService} from '../utils/services/auth.service';
+
+export function resolveGoogleAuthClient(AuthService: AuthService) {
+  return () => AuthService.loadClient();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideClientHydration(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: true,
+      autoPause: true,
+    }),
+    AuthService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: resolveGoogleAuthClient,
+      deps: [AuthService],
+      multi: true,
+    },
   ],
 };
