e8b351376eda46cbea9eb2324a31a205ba9b5a28
gustavo <gustavo.passarella@hotmail.com>
2024-11-19 11:17:51 +0000

    Move routes to app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+import {LOGIN_PATH, LOGGED_IN_PATH} from '../utils/routes';
+
+export const routes: Routes = [
+  {path: '', redirectTo: LOGIN_PATH, pathMatch: 'full'},
+  // {path: LOGIN_PATH, component: LoginComponent},
+  // {path: LOGGED_IN_PATH, component: LoggedInComponent},
+];
