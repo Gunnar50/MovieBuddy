45ce47b02eb33ad264f02c460443e9680989c35f
gustavo <gustavo.passarella@hotmail.com>
2024-11-26 13:30:55 +0000

    Update constants

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -3,7 +3,7 @@ import os
 SECRET_ID = 'client-config'
 SESSION_USER_ID = 'user_id'
 
-GAE_PROJECT = os.getenv('GOOGLE_CLOUD_PROJECT')
+GAE_PROJECT = os.getenv('GOOGLE_CLOUD_PROJECT', '')
 GAE_ENV = os.environ.get('GAE_ENV')
 IS_LOCAL = (GAE_ENV is None or GAE_ENV == 'localdev')
 LOCAL_DEV = os.environ.get('LOCAL_DEV', None)
